2ee5f2b65ace668f6dd0ab1f9c9f0935b8341342
alfernabreu <86300297+alfernabreu@users.noreply.github.com>
2022-12-15 15:39:30 -0300

    problema com identificação de string resolvido

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 simbolos = ["{", "}", "[", "]", "(", ")", "?", "/", ";", ":", ".", ">", "<", ",", "|", "\\"]
 
 print("Bem-vindo!")
-
-num_caracteres = 0
-num_caracteres = int(input("Por favor, informe o número de caracteres que deve compor a sua senha:\n"))
-if num_caracteres < 8 or num_caracteres is str:
-    num_caracteres = int(input("É recomendado que o número de caracteres seja um número inteiro igual ou maior que 8.\nPor favor, informe o número de caracteres que deve compor a sua senha:\n"))
-
+num_caracteres = (input("Por favor, informe o número de caracteres (entre 8 e 32) que deve compor a sua senha:\n"))
+if num_caracteres.isdigit():
+    num_caracteres = int(num_caracteres)
+    while num_caracteres < 8 or num_caracteres > 32:
+            if num_caracteres < 8:
+                print("Você digitou um número menor do que o recomendado.")
+                num_caracteres = int((input("Por favor, informe o número de caracteres (entre 8 e 32) que deve compor a sua senha:\n")))
+            elif num_caracteres > 32:
+                print("Você digitou um número maior que o recomendado.")
+                num_caracteres = int((input("Por favor, informe o número de caracteres (entre 8 e 32) que deve compor a sua senha:\n")))
+    print(f"Muito bem. Você escolheu uma senha contendo {num_caracteres} caracteres.")
+else:
+    print("Isso não é um número.\nReinicie o programa.")
 
 adicionar_numeros = input("Agora, informe se sua senha deve conter números (Sim/Não):\n").lower
 qnt_numero = 0
