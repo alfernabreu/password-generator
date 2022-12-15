02604efb8d163c84a9f751097d08dcd85a1f8fe0
alfernabreu <86300297+alfernabreu@users.noreply.github.com>
2022-12-15 08:57:02 -0300

    Dialogo com o usuário adicionado

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", 
 "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
 simbolos = ["{", "}", "[", "]", "(", ")", "?", "/", ";", ":", ".", ">", "<", ",", "|", "\\"]
+
+print("Bem-vindo!")
+num_caracteres = input("Por favor, informe o número de caracteres que deve compor a sua senha:\n")
+adicionar_numeros = input("Agora, informe se sua senha deve conter números (Sim/Não):\n").lower
+adicionar_simbolos = input("Por fim, informe se deseja que sua senha contenha simbos (Sim/Não:\n").lower
