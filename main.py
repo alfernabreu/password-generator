c4767f47253f245ddc8928c269d0f653002b55d9
alfernabreu <86300297+alfernabreu@users.noreply.github.com>
2022-12-19 19:19:50 -0300

    Resolvido problema com estrutura condicional no segundo input

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ simbolos = ["{", "}", "[", "]", "(", ")", "?", "/", ";", ":", ".", ">", "<", ","
 print("Bem-vindo!")
 num_caracteres = (input("Por favor, informe o número de caracteres (entre 8 e 32) que deve compor a sua senha:\n"))
 check = num_caracteres.isdigit()
+
 while check == False:
     num_caracteres = input('Isso não é um número.\nPor favor, informe o número de caracteres(entre 8 e 32) que deve compor a sua senha:\n')
     check = num_caracteres.isdigit()
+
 num_caracteres = int(num_caracteres)
 while num_caracteres < 8 or num_caracteres > 32:
         if num_caracteres < 8:
@@ -19,4 +21,14 @@ while num_caracteres < 8 or num_caracteres > 32:
         elif num_caracteres > 32:
             print("Você digitou um número maior que o recomendado.")
             num_caracteres = int((input("Por favor, informe o número de caracteres (entre 8 e 32) que deve compor a sua senha:\n")))
+
 print(f"Muito bem. Você escolheu uma senha contendo {num_caracteres} caracteres.")
+
+qnt_numero = 0
+adicionar_numeros = input("\nAgora, informe se sua senha deve conter números (Sim/Não):\n").lower()
+while adicionar_numeros != "sim" and adicionar_numeros != "não":
+    adicionar_numeros = input('Você deve digitar "Sim" ou Não" para concluir esta etapa.\nAgora, informe se sua senha deve conter números:\n').lower()
+if adicionar_numeros == "sim":
+    qnt_numero = int(input("Você optou por adicionar números a sua senha.\nInforme quantos números devem compor sua senha:\n"))
+
+adicionar_simbolos = input("\nPor fim, informe se deseja que sua senha contenha simbolos (Sim/Não:\n").lower()
